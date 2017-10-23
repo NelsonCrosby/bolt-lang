f2f4afda6525d3dae366dd4dd80906ce7eb93747
Nelson Crosby <ncrosby@sourcecomb.com>
2017-10-23 13:09:35 +1300

    Reset and prepare for using mpc

--- src/boltc/lex/lexer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct lexer lexer_t;
-
-lexer_t *lex_new_mem(size_t len, const char *src);
-void lex_free(lexer_t *lex);
-
-int lex_step(lexer_t *lex);
-token_t *lex_next(lexer_t *lex);
